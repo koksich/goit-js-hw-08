d5e71c973bd7cdcbff057963da530c6a04864b52
Natasha Kuksa <natasha.kuksa.96@gmail.com>
2023-03-12 13:55:19 +0100

    Task-01, task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds));
+  }, 1000)
+);
+
+const currentTime = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
